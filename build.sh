92ea6844dd42cff327be9388eea88ce2a1f8281d
Matheus Nascimento <contamatheusnascimento@gmail.com>
2018-04-22 22:53:17 -0300

    Updates the build script

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 clear 
-cd ~/python_sensor/
-# rm PyQT/pysensor.py PyQT/matplot.py PyQT/aboutscreen.py
+cd ~/rospy_sensor_monitor/
 rm PyQT/pysensor.py 
 pyuic4 QT/pyino_sensor_display/pysensormain.ui -o PyQT/pysensor.py 
-# pyuic4 QT/pyino_sensor_display/matplot.ui -o PyQT/matplot.py 
-# pyuic4 QT/pyino_sensor_display/aboutscreen.ui -o PyQT/aboutscreen.py 
-# pyuic4 QT/pyino_sensor_display/frame.ui -o PyQT/frame.py 
 rm PyQT/pyino_sources_rc.py
 pyrcc4 -o PyQT/pyino_sources_rc.py QT/pyino_sensor_display/pyino_sources.qrc
 cd PyQT/ && python main.py
