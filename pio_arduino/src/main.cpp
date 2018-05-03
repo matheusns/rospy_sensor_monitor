4d8620f9dddeb53db3ef1a160dd875795f855549
Matheus Nascimento <contamatheusnascimento@gmail.com>
2018-05-03 15:13:57 -0300

    adds ros main

--- pio_arduino/src/main.cpp
+++ pio_arduino/src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include <ros.h>
+#include <std_msgs/Float32.h>
 
-void setup() {
-    // put your setup code here, to run once:
+int sensorPin = A0;
+ros::NodeHandle nh;
+std_msgs::Float32 tempC;
+ros::Publisher pub("sensor_temperature", &tempC);
+
+void setup()
+{
+  Serial.begin(9600);  
+  nh.initNode();
+  nh.advertise(pub);
 }
+ 
+void loop()                     
+{
+ int reading = analogRead(sensorPin);  
+ float voltage = reading * 5.0;
+ voltage /= 1024.0; 
+ 
+ Serial.print(voltage); Serial.println(" volts");
+ 
+ float temperatureC = (voltage - 0.5) * 100 ;  
+ Serial.print(temperatureC); 
+ Serial.println(" degrees C");
+ 
+ tempC.data = temperatureC; 
+ pub.publish(&tempC);
+ nh.spinOnce();
 
-void loop() {
-    // put your main code here, to run repeatedly:
+ delay(2000);                                    
 }
